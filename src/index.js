2db6c099212f067271518ebb1c6f24d9cc035ed5
weinuo <weinuo@jingzhi-sh.com>
2024-06-13 12:04:14 +0800

    更新url

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.e-whisper.com": "https://registry-1.docker.io",
-  "quay.e-whisper.com": "https://quay.io",
-  "gcr.e-whisper.com": "https://gcr.io",
-  "k8s-gcr.e-whisper.com": "https://k8s.gcr.io",
-  "k8s.e-whisper.com": "https://registry.k8s.io",
-  "ghcr.e-whisper.com": "https://ghcr.io",
-  "cloudsmith.e-whisper.com": "https://docker.cloudsmith.io",
+  "docker.weinuo.com": "https://registry-1.docker.io",
+  "quay.weinuo.com": "https://quay.io",
+  "gcr.weinuo.com": "https://gcr.io",
+  "k8s-gcr.weinuo.com": "https://k8s.gcr.io",
+  "k8s.weinuo.com": "https://registry.k8s.io",
+  "ghcr.weinuo.com": "https://ghcr.io",
+  "cloudsmith.weinuo.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
